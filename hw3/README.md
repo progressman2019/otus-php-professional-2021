5b56030609af4ccf921d72e696041a960eae92ca
progressman2019 <progressman2019@gmail.com>
2022-02-18 16:55:37 +0600

    first commit

---
+++ hw3/README.md
@@ -0,0 +1,15 @@
+[Otus. PHP Developer. Professional 2021](https://otus.ru/lessons/razrabotchik-php/?int_source=courses_catalog&int_term=programming)
+==============================
+
+## Домашнее задание №3
+
+#### Необходимо создать свой пакет.
+
+В pull-request прислать composer.json, в котором приводится пример подключения Вашего пакета.
+
+<br>
+
+>Критерии оценки:
+>- Пакет должен ставиться при помощи composer require package-name;
+>- Пакет должен отвечать PSR-4;
+>- Пакет может подключаться в Composer либо с packagist, либо из GitHub;
---
+++ hw5-2/repcahed/app/src/App.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Memcached;
+
+class App
+{
+    public function run(): void
+    {
+        session_start();
+
+        echo 'Session_id: ' . session_id() . '<br>';
+        echo 'Nginx: ' . $_SERVER['SERVER_ADDR'] . '<br>';
+        echo 'Php-fpm: ' . getHostByName(php_uname('n')) . '<br>';
+
+        $this->testMemcached();
+    }
+
+    private function testMemcached(): void
+    {
+        $mem = new Memcached();
+
+        $mem->addServer('mysite-memcached-2', 11211);
+
+        $key = 'test1';
+        $value = 'mem1';
+        $result = $mem->set($key, $value);
+
+        echo "Memcached. Set: " . ($result ? "{$key}={$value}" : 'value is not set') . '<br>';
+        echo "Memcached. Get: {$key}=" . $mem->get($key) . '<br>';
+    }
+}
